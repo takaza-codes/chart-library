efc0ff4f901e9e3069fe5067792f9ed0a3e5d5e3
takaza-codes <elekovalenko@gmail.com>
2025-04-22 23:47:05 +0300

    add pie chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Chart } from "react-google-charts";
 
 function App() {
+  const data = [
+    ["LLM model", "Percentage"],
+    ["Mistral", 4.7],
+    ["Gemini", 10.3],
+    ["Claude", 14.0],
+    ["GPT-4", 27.2],
+    ["Llama", 43.9],
+  ];
+
+  const options = {
+    title: "LLM Usage in June, 2024",
+    pieHole: 0.4,
+    is3D: true,
+    slices: {
+      0: { offset: 0.2 },
+    },
+    pieStartAngle: 100,
+    sliceVisibilityThreshold: 0.02,
+    legend: {
+      position: "bottom",
+      alignment: "center",
+      textStyle: {
+        color: "#233238",
+        fontSize: 14,
+      },
+    },
+    colors: ["#8AD1C2", "#9F8AD1", "#D18A99", "#BCD18A", "#D1C28A"],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
 
